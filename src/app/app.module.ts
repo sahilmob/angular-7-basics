73bc099b016b32fddf54f614c0f1ed79a34dbb24
Sahil Mobaidin <sahil@localhost.localdomain>
2019-05-07 14:59:57 +0300

    use servers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ServerComponent } from './server/server.component'
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
