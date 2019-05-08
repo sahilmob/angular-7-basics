4cd4de6f77a85ffe36fcb197aa33c4137f433347
Sahil Mobaidin <sahil@localhost.localdomain>
2019-05-08 12:02:30 +0300

    add assignment components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component'
+import {WarningAlert} from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlert,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
